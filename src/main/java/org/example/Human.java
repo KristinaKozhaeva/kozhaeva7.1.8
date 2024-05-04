2c07d88f9a6632bcfd66b7541c6affc2340b5ad8
Kozhaeva Kristina <149185195+KristinaKozhaeva@users.noreply.github.com>
2024-05-04 19:35:08 +0300

    Моя первая попытка

---
+++ src/main/java/org/example/Human.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Human {
+    String firstName;
+    String lastName;
+}
